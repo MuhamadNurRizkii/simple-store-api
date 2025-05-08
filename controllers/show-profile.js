b18c88fd6e833d0c84ef2c17fc1ebff0d769babc
Muhamad Nur Rizki <farianhab123@gmail.com>
2025-05-08 20:47:49 +0700

    membuat kode showProfile

--- controllers/show-profile.js
+++ controllers/show-profile.js
@@ -3,25 +3,45 @@ const connectDB = require("../config/database");
 
 // fungsi get data profil
 const showProfile = async (req, res) => {
-  const db = await connectDB();
-  const { id } = req.params;
-  const collection = db.collection("users");
-  console.log(id);
+  try {
+    const db = await connectDB();
+    const { id } = req.params;
+    const collection = db.collection("users");
+    console.log(id);
 
-  if (!id) {
-    return res.status(400).json({
-      message: "Username tidak ditemukan!",
+    if (!id) {
+      return res.status(400).json({
+        message: "Username tidak ditemukan!",
+      });
+    }
+
+    let objectId;
+    try {
+      objectId = new ObjectId(id);
+    } catch (err) {
+      return res.status(400).json({
+        message: "Format ID tidak valid!",
+      });
+    }
+
+    const dataUser = await collection.findOne({ _id: objectId });
+
+    if (!dataUser) {
+      return res.status(404).json({
+        message: "User tidak ditemukan!",
+      });
+    }
+
+    res.json({
+      data: {
+        nama_lengkap: dataUser.nama_lengkap,
+        username: dataUser.username,
+      },
     });
+  } catch (err) {
+    console.log("Error:", err);
+    res.status(500).json({ message: "Server Error", error: err.message });
   }
-
-  const dataUser = await collection.findOne({ _id: new ObjectId(id) });
-
-  res.json({
-    data: {
-      nama_lengkap: dataUser.nama_lengkap,
-      username: dataUser.username,
-    },
-  });
 };
 
 module.exports = showProfile;
