23652696e46036801f62ddc4618249e7ac1c1158
Muhamad Nur Rizki <farianhab123@gmail.com>
2025-05-10 09:07:40 +0700

    remove multer

--- api/server.js
+++ api/server.js
@@ -14,7 +14,6 @@ const port = process.env.PORT || 4000;
 // middleware
 app.use(cors());
 app.use(express.json());
-app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 // routing
 app.use("/", router);
